ef40d172f0542a5fe602fccb66eff6eea9a58a03
liuhuatao <597897040@qq.com>
2018-03-11 15:03:43 +0800

    api cors配置

--- src/controller/api/category.js
+++ src/controller/api/category.js
@@ -13,17 +13,9 @@ module.exports = class extends think.cmswing.rest {
       return this.success(data);
     }
     data = await model.get_all_category();
-    this.setCorsHeader();
     return this.success(arr_to_tree(data, 0));
   }
   async postAction() {
-    this.setCorsHeader();
     return this.success(this.post());
   }
-  setCorsHeader(){
-    this.header("Access-Control-Allow-Origin", this.header("origin") || "*");
-    this.header("Access-Control-Allow-Headers", "x-requested-with");
-    this.header("Access-Control-Request-Method", "GET,POST,PUT,DELETE");
-    this.header("Access-Control-Allow-Credentials", "true");
-    }
 };
